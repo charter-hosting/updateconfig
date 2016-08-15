22e6108e1f83ef24203acdd3efa1436dbc06c933
Mike M <agentmoller001@gmail.com>
2016-08-15 10:02:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /web
+cd /web/test
 
-echo Creating log file wpupdate.log
+echo "Creating log file wpupdate.log"
 touch wpupdate.log
 
-echo Writing update script to file
+echo "Writing update script to file"
 cat <<EOF >wpupdate.sh
 #!/bin/bash
 timestamp() {
@@ -21,29 +21,22 @@ wp core update
 exit 0
 EOF
 
-echo Setting .htaccess rule to block direct access to update log and script file
-cat <<EOF >.htaccess
-<FilesMatch "wpupdate\.log|wpupdate\.sh">
-    Order Allow,Deny
-    Allow from 127.0.0.1
-    Deny from all
-</FilesMatch>
-EOF
+echo "Setting .htaccess rule to block direct access to update log and script file"
+echo "<FilesMatch \"wpupdate\.log|wpupdate\.sh\">" >> .htaccess
+echo "Order Allow,Deny" >> .htaccess
+echo "Allow from 127.0.0.1" >> .htaccess
+echo "Deny from all" >> .htaccess
+echo "</FilesMatch>" >> .htaccess
 
-echo Creating sample data wp-config-data.txt file
-cat <<EOF >wp-config-data.txt
-/** Tell WP-CLI to use TCP instead of socket connection */
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-/** MySQL hostname for WP-CLI */
-define('DB_HOST', '127.0.0.1:3306');
-} else {
-/** MySQL hostname */
-('DB_HOST', 'localhost'); }
-EOF
+echo "Configuration Complete!
+echo "Don't forget to update the wp-config.php file to use TCP.
+echo
+echo "/** Tell WP-CLI to use TCP instead of socket connection */"
+echo "if ( defined( 'WP_CLI' ) && WP_CLI ) {"
+echo "/** MySQL hostname for WP-CLI */"
+echo "define('DB_HOST', '127.0.0.1:3306');"
+echo "} else {"
+echo "/** MySQL hostname */"
+echo "('DB_HOST', 'localhost'); }"
 
-chmod 0644 wp-config-data.txt
-
-echo Configuration Complete!
-echo Don't forget to update the wp-config.php file to use TCP.
-echo Sample data in the wp-config-data.txt
 exit 0
